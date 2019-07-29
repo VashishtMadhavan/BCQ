3da0e9031dcc7fc48c6c3215ad62b3ce6f64d770
Vashisht Madhavan <vashisht@uber.com>
2019-07-29 12:58:38 -0700

    switching to Gaussian policy so we can compute entropy for ensemble

--- TD3.py
+++ TD3.py
@@ -2,25 +2,39 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.distributions import Normal
 import utils
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Implementation of Twin Delayed Deep Deterministic Policy Gradients (TD3)
 # Paper: https://arxiv.org/abs/1802.09477
+
+LOG_STD_MIN = -20
+LOG_STD_MAX = 2
 
 class Actor(nn.Module):
 	def __init__(self, state_dim, action_dim, max_action):
 		super(Actor, self).__init__()
 		self.l1 = nn.Linear(state_dim, 400)
 		self.l2 = nn.Linear(400, 300)
-		self.l3 = nn.Linear(300, action_dim)
+		self.l3 = nn.Linear(300, action_dim * 2)
 		self.max_action = max_action
 
-	def forward(self, x):
+	def mu_var(self, x):
 		x = F.relu(self.l1(x))
 		x = F.relu(self.l2(x))
-		x = self.max_action * torch.tanh(self.l3(x)) 
-		return x
+		x = self.l3(x)
+		mu, log_std = torch.split(x, x.size(1) // 2, dim=1)
+
+		log_std = torch.tanh(log_std)
+		log_std = LOG_STD_MIN + 0.5 * (LOG_STD_MAX - LOG_STD_MIN) * (log_std + 1)
+		std = torch.exp(log_std)
+		return mu, std
+
+	def forward(self, x):
+		mu, std = self.mu_var(x)
+		normal = Normal(mu, std)
+		act = torch.tanh(normal.rsample())
+		return self.max_action * act
 
 class EnsembleActor(nn.Module):
 	def __init__(self, state_dim, action_dim, max_action, K=5):
@@ -86,18 +100,19 @@ class EnsembleCritic(nn.Module):
 		return torch.stack(qs)
 
 class TD3(object):
-	def __init__(self, state_dim, action_dim, max_action, K=1):
+	def __init__(self, state_dim, action_dim, max_action, device, K=1):
 		self.K = K
+		self.device = device
 		if self.K == 1:
-			self.actor = Actor(state_dim, action_dim, max_action).to(device)
-			self.actor_target = Actor(state_dim, action_dim, max_action).to(device)
-			self.critic = Critic(state_dim, action_dim).to(device)
-			self.critic_target = Critic(state_dim, action_dim).to(device)
+			self.actor = Actor(state_dim, action_dim, max_action).to(self.device)
+			self.actor_target = Actor(state_dim, action_dim, max_action).to(self.device)
+			self.critic = Critic(state_dim, action_dim).to(self.device)
+			self.critic_target = Critic(state_dim, action_dim).to(self.device)
 		else:
-			self.actor = EnsembleActor(state_dim, action_dim, max_action, K=self.K).to(device)
-			self.actor_target = EnsembleActor(state_dim, action_dim, max_action, K=self.K).to(device)
-			self.critic = EnsembleCritic(state_dim, action_dim, K=self.K).to(device)
-			self.critic_target = EnsembleCritic(state_dim, action_dim, K=self.K).to(device)
+			self.actor = EnsembleActor(state_dim, action_dim, max_action, K=self.K).to(self.device)
+			self.actor_target = EnsembleActor(state_dim, action_dim, max_action, K=self.K).to(self.device)
+			self.critic = EnsembleCritic(state_dim, action_dim, K=self.K).to(self.device)
+			self.critic_target = EnsembleCritic(state_dim, action_dim, K=self.K).to(self.device)
 
 		self.actor_target.load_state_dict(self.actor.state_dict())
 		self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
@@ -107,7 +122,7 @@ class TD3(object):
 
 	def select_action(self, state):
 		with torch.no_grad():
-			state = torch.FloatTensor(state).to(device)
+			state = torch.FloatTensor(state).to(self.device)
 			if self.K == 1:
 				return self.actor(state).cpu().numpy()
 			else:
@@ -115,27 +130,54 @@ class TD3(object):
 				total_actions = self.actor(state).cpu().numpy()
 				return total_actions[ensemble_idx]
 
+	# Estimates Jensen-Renyi divergence for ensemble mixture of gaussians
+	def _jrd(self, state):
+		with torch.no_grad():
+			mu, var = self.actor(state) # [B, K, A]
+		mu = torch.tanh(mu) * self.actor.max_action # TODO: confirm that this is correcy
+		mu_diff = mu.unsqueeze(1) - mu.unsqueeze(2)
+		var_sum = var.unsqueeze(1) + var.unsqueeze(2)
+		n_act, es, a_s = mu.size()
+
+		err = (mu_diff * 1 / var_sum * mu_diff)
+		err = torch.sum(err, dim=-1)
+		det = torch.sum(torch.log(var_sum), dim=-1)
+
+		log_z = -0.5 * (err + det)
+		log_z = log_z.reshape(n_act, es * es)
+		mx, _ = log_z.max(dim=1, keepdim=True)
+		log_z = log_z - mx
+		exp = torch.exp(log_z).mean(dim=1, keepdim=True)
+		ent_mean = -mx - torch.log(exp)
+		ent_mean = entropy_mean[:, 0]
+
+		# mean of entropies
+		total_ent = torch.sum(torch.log(var), dim=-1)
+		mean_ent = total_ent.mean(dim=1) / 2 + a_s * np.log(2) / 2
+		return ent_mean - mean_ent
+
+
 	def train(self, replay_buffer, iterations, batch_size=100, discount=0.99, tau=0.005, 
 		policy_noise=0.2, noise_clip=0.5, policy_freq=2, priority=False, total_steps=1000):
 		for it in range(iterations):
 			# Sample replay buffer 
 			if priority:
 				state, next_state, act, reward, done, weights, idxes = replay_buffer.sample(batch_size, total_steps)
-				weights = torch.FloatTensor(weights).to(device)
+				weights = torch.FloatTensor(weights).to(self.device)
 			else:
 				state, next_state, act, reward, done = replay_buffer.sample(batch_size)
-			state 		= torch.FloatTensor(state).to(device)
-			action 		= torch.FloatTensor(act).to(device)
-			next_state 	= torch.FloatTensor(next_state).to(device)
-			reward 		= torch.FloatTensor(reward).to(device)
-			done 		= torch.FloatTensor(1 - done).to(device)
+			state 		= torch.FloatTensor(state).to(self.device)
+			action 		= torch.FloatTensor(act).to(self.device)
+			next_state 	= torch.FloatTensor(next_state).to(self.device)
+			reward 		= torch.FloatTensor(reward).to(self.device)
+			done 		= torch.FloatTensor(1 - done).to(self.device)
 
 			# Select action according to policy and add clipped noise
 			if self.K == 1:
-				noise = torch.FloatTensor(act).data.normal_(0, policy_noise).to(device)
+				noise = torch.FloatTensor(act).data.normal_(0, policy_noise).to(self.device)
 			else:
 				ens_act = np.repeat(np.expand_dims(act, 0), self.K, axis=0)
-				noise = torch.FloatTensor(ens_act).data.normal_(0, policy_noise).to(device)
+				noise = torch.FloatTensor(ens_act).data.normal_(0, policy_noise).to(self.device)
 			noise = noise.clamp(-noise_clip, noise_clip)
 			next_action = (self.actor_target(next_state) + noise).clamp(-self.max_action, self.max_action)
 
@@ -146,19 +188,27 @@ class TD3(object):
 
 			# Get current Q estimates
 			current_Q1, current_Q2 = self.critic(state, action)
+			td_error = torch.abs(current_Q1 - target_Q) + torch.abs(current_Q2 - target_Q)
 
 			# Compute critic loss
 			critic_loss = F.mse_loss(current_Q1, target_Q, reduction='none') + F.mse_loss(current_Q2, target_Q, reduction='none')
 
-			# Update priorities with TD error
-			replay_buffer.update_priorities(idxes, critic_loss.detach().cpu().numpy().squeeze())
+			# Updating priorities
+			if priority:
+				if self.K == 1:
+					priorities = td_error.detach().cpu().numpy().squeeze()
+				else:
+					# Estimating Jensen-Renyi divergence between policies
+					priorities = self._jrd(state)
+					critic_loss = torch.mean(critic_loss, dim=0)
+				replay_buffer.update_priorities(idxes, priorities)
+				critic_loss = torch.mean(weights * critic_loss)
+			else:
+				critic_loss = torch.mean(critic_loss)
 
 			# Optimize the critic
 			self.critic_optimizer.zero_grad()
-			if priority:
-				torch.mean(weights * critic_loss).backward()
-			else:
-				torch.mean(critic_loss).backward()
+			critic_loss.backward()
 			self.critic_optimizer.step()
 
 			# Delayed policy updates
