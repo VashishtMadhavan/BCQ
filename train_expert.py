3b7e30bc6e78d74ec5f86ecb67bb0287073ce5c8
Vashisht Madhavan <vashisht@uber.com>
2019-07-29 12:58:01 -0700

    cleaning up training script

--- train_expert.py
+++ train_expert.py
@@ -13,11 +13,11 @@ import TD3
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--env_name", default="Ant-v2")				# OpenAI gym environment name
-	parser.add_argument("--algo", default='TD3', choices=['DDPG', 'TD3'], type=str) # which algo to use
 	parser.add_argument("--seed", default=0, type=int)					# Sets Gym, PyTorch and Numpy seeds
 	parser.add_argument("--max_timesteps", default=1e6, type=float)		# Max time steps to run environment for
 	parser.add_argument("--start_timesteps", default=1e4, type=int)		# How many time steps purely random policy is run for
 	parser.add_argument("--test_eps", default=10, type=int)             # Number of evaluation episodes to run
+	parser.add_argument("--gpu", default=0, type=int)					# Which GPU to train the model on; -1 for CPU
 
 	# training parameters
 	parser.add_argument("--expl_noise", default=0.1, type=float)		# Std of Gaussian exploration noise
@@ -27,11 +27,10 @@ if __name__ == "__main__":
 	parser.add_argument("--K", default=1, type=int)						# Number of ensemble members
 
 	# prioritized replay params
-	parser.add_argument("--priority", action="store_false")				# Whether or not to use prioritization
+	parser.add_argument("--priority", action="store_true")				# Whether or not to use prioritization
 	parser.add_argument("--alpha", default=0.6, type=float)
 	parser.add_argument("--beta", default=0.4, type=float)
 	parser.add_argument("--eps", default=1e-6, type=float)
-
 
 	# TD3 specific parameters
 	parser.add_argument("--policy_noise", default=0.2, type=float)		# Noise added to target policy during critic update
@@ -39,13 +38,19 @@ if __name__ == "__main__":
 	parser.add_argument("--policy_freq", default=2, type=int)			# Frequency of delayed policy updates
 	args = parser.parse_args()
 
-	file_name = "%s_%s_%s" % (args.algo, args.env_name, str(args.seed))
+	file_name = "TD3_%s_%s" % (args.env_name, str(args.seed))
 	print("---------------------------------------")
 	print("Settings: " + file_name)
 	print("---------------------------------------")
 
 	if not os.path.exists("./pytorch_models"):
 		os.makedirs("./pytorch_models")
+
+	if torch.cuda.is_available():
+		torch.cuda.set_device(args.gpu)
+		device = torch.device("cuda")
+	else:
+		device = torch.device("cpu")
 
 	# Saving config parameters
 	config_file_name = file_name + "_config.json"
@@ -66,11 +71,7 @@ if __name__ == "__main__":
 	max_action = float(env.action_space.high[0])
 
 	# Initialize policy and buffer
-	if args.algo == 'TD3':
-		policy = TD3.TD3(state_dim, action_dim, max_action, K=args.K)
-	else:
-		policy = DDPG.DDPG(state_dim, action_dim, max_action, K=args.K)
-
+	policy = TD3.TD3(state_dim, action_dim, max_action, device, K=args.K)
 	if args.priority:
 		replay_buffer = utils.PriorityReplayBuffer(timesteps=args.max_timesteps, alpha=args.alpha, beta=args.beta, eps=args.eps)
 	else:
@@ -89,11 +90,8 @@ if __name__ == "__main__":
 	while total_timesteps < args.max_timesteps:
 		if done: 
 			if total_timesteps != 0:
-				if args.algo == 'TD3':
-					policy.train(replay_buffer, episode_timesteps, args.batch_size, args.discount,
-						args.tau, args.policy_noise, args.noise_clip, args.policy_freq, args.priority, total_timesteps)
-				else:
-					policy.train(replay_buffer, episode_timesteps, args.batch_size, args.discount, args.tau)
+				policy.train(replay_buffer, episode_timesteps, args.batch_size, args.discount,
+					args.tau, args.policy_noise, args.noise_clip, args.policy_freq, args.priority, total_timesteps)
 
 			# Evaluate and Save Policy
 			if total_episodes % 1 == 0:
